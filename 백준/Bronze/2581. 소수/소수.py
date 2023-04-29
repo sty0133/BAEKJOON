6cf1ddeaf1b7061a14685ea384ea98d0724470e3
Hwangbo Jaeyun <72016056+sty0133@users.noreply.github.com>
2023-04-29 17:45:47 +0900

    [Bronze II] Title: 소수, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -9,7 +9,7 @@ for i in range(m,n+1):
     count = 0
     if i > 1:
         # primenum.append(i)
-        for j in range(2,i):
+        for j in range(2,int(i**0.5)+1):
             if i % j == 0:
                 count = 1
                 break
