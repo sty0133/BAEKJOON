6ba4c21fbfa6444e8f25161cb367c303ce312811
Hwangbo Jaeyun <72016056+sty0133@users.noreply.github.com>
2023-05-15 17:04:55 +0900

    [Silver V] Title: 분수찾기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,20 @@
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+
+line = 0
+end = 0
+while n > end:
+    line += 1
+    end += line
+
+diff = end - n
+if line % 2 == 0:
+    top = line - diff
+    bottom = diff + 1
+else:
+    top = diff + 1
+    bottom = line - diff
+
+print("%d/%d"%(top,bottom))
